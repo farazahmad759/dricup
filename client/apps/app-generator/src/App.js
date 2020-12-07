9c8d5a917e580687da37fd26b05a15b8e2686520
Faraz Ahmad <farazahmad759@gmail.com>
2020-12-07 19:36:47 +0500

    Made some changes

--- client/apps/app-generator/src/App.js
+++ client/apps/app-generator/src/App.js
@@ -2,27 +2,60 @@ import logo from "./logo.svg";
 import JSZip from "jszip";
 import saveAs from "file-saver";
 import { useEffect, useState } from "react";
+import axios from "axios";
 // var zip = new JSZip();
 
 import "./App.css";
 
 function App() {
-  const [page, setPage] = useState(null);
+  const [generatorConfig, setGeneratorConfig] = useState({});
   useEffect(() => {});
   const downloadProject = () => {
-    var zip = new JSZip();
-    zip.file("package.json", "hello");
-    zip.generateAsync({ type: "blob" }).then(function (content) {
-      saveAs(content, "archive.zip");
+    axios.get("http://localhost:8000/api/files").then((res) => {
+      let files = res.data.data;
+      if (files) {
+        var zip = new JSZip();
+        files.forEach((file) => {
+          let _content = generateContent(
+            JSON.parse(file.content_vars),
+            file.content
+          );
+          zip.file(file.path, _content);
+        });
+        zip.generateAsync({ type: "blob" }).then(function (content) {
+          saveAs(content, "archive.zip");
+        });
+      }
     });
   };
+  function generateContent(options, content) {
+    let _content = "`" + content + "`";
+    _content = new Function("options", "return `" + content + "`").call(
+      content,
+      options
+    );
+    _content = `
+    {
+      "hello": "hi",
+      ${options.knex && '"knex": "1.2.3"'}
+    }
+    `;
+    return _content;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <button onClick={downloadProject}>Download</button>
+        <button
+          onClick={() => {
+            downloadProject();
+          }}
+        >
+          Download
+        </button>
       </header>
     </div>
   );
